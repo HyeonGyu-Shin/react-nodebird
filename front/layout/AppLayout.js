6385177124d6f6099fb1d1fdac55d6d893efa2a1
hyeongyu shin <rkrkdldkd@gmail.com>
2022-03-22 01:29:49 +0900

    💄 Implement grid  through row,col in antd

--- front/layout/AppLayout.js
+++ front/layout/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -18,12 +18,28 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search
+            enterButton
+            style={{ verticalAlign: "middle" }}
+          ></Input.Search>
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          오른쪽 메뉴
+        </Col>
+      </Row>
     </div>
   );
 };
