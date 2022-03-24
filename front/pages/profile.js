58770806b9c78c9bf9e334b96592b5bbda6d928c
hyeongyu shin <rkrkdldkd@gmail.com>
2022-03-25 00:34:53 +0900

    🩹 Change the position of the Head tag

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -18,10 +18,10 @@ const Profile = () => {
   ];
   return (
     <>
-      <Head>
-        <title>내 프로필 | NodeBird</title>
-      </Head>
       <AppLayout>
+        <Head>
+          <title>내 프로필 | NodeBird</title>
+        </Head>
         <NicknameEditForm />
         <FollowList header="팔로잉 목록" data={followingList} />
         <FollowList header="팔로워 목록" data={followerList} />
