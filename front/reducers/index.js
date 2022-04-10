7a5fc8ce84a02368dd146662f7834192ad89ccc5
hyeongyu shin <rkrkdldkd@gmail.com>
2022-04-10 12:12:11 +0900

    🚧 Implement rootReducer

---
+++ front/reducers/index.js
@@ -0,0 +1,46 @@
+const initialState = {
+    user: {
+        isLoggedIn: false,
+        user: null,
+    },
+};
+
+export const LogInAction = (data) => {
+    return {
+        type: "Log_In_Action",
+        data: data,
+    };
+};
+
+export const LogOutAction = () => {
+    return {
+        type: "Log_Out_Action",
+    };
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "Log_In_Action":
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    isLoggedIn: true,
+                    user: action.data,
+                },
+            };
+        case "Log_Out_Action":
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    isLoggedIn: false,
+                    user: null,
+                },
+            };
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
