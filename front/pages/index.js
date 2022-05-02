54d7ad256cb48ab2976250969d238026b2204e64
hyeongyu shin <rkrkdldkd@gmail.com>
2022-05-03 01:09:04 +0900

    🚧 Implement PostCard component

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,20 @@
-import React from "react";
-import AppLayout from "../layout/AppLayout";
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import AppLayout from '../layout/AppLayout';
+import PostForm from '../components/post/postForm';
+import PostCard from '../components/post/postCard';
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+
   return (
     <AppLayout>
-      <div>Hello, Next!</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
