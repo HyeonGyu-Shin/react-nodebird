9c3978bb55c59453146d14075704d30ef5a3a3b6
hyeongyu shin <rkrkdldkd@gmail.com>
2022-05-03 02:53:43 +0900

    ♻️ Change initialState from user to me

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,40 +1,40 @@
 const initialState = {
-    isLoggedIn: false,
-    user: null,
-    signUpData: {},
-    loginData: {},
+  isLoggedIn: false,
+  me: null,
+  signUpData: {},
+  loginData: {},
 };
 
 export const LogInAction = (data) => {
-    return {
-        type: "Log_In_Action",
-        data: data,
-    };
+  return {
+    type: 'Log_In_Action',
+    data: data,
+  };
 };
 
 export const LogOutAction = () => {
-    return {
-        type: "Log_Out_Action",
-    };
+  return {
+    type: 'Log_Out_Action',
+  };
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "Log_In_Action":
-            return {
-                ...state,
-                isLoggedIn: true,
-                user: action.data,
-            };
-        case "Log_Out_Action":
-            return {
-                ...state,
-                isLoggedIn: false,
-                user: null,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'Log_In_Action':
+      return {
+        ...state,
+        isLoggedIn: true,
+        me: action.data,
+      };
+    case 'Log_Out_Action':
+      return {
+        ...state,
+        isLoggedIn: false,
+        me: null,
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
