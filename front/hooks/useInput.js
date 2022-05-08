4eaa10c901a965a237302c17a4e46ff3a9cc4816
hyeongyu shin <rkrkdldkd@gmail.com>
2022-05-08 20:56:33 +0900

    🩹 fix warning about useInput

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -1,10 +1,12 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
-export default (initialValue = null) => {
+const hooks = (initialValue = null) => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
-  return [value, handler];
+  return [value, handler, setValue];
 };
+
+export default hooks;
